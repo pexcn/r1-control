8741ccc5e92038980f33e6ed856a2d6ae8bc47cc
小飞 <w22649@139.com>
2022-04-16 15:13:17 +0000

    update new_r1_control.js.

--- new_r1_control.js
+++ new_r1_control.js
@@ -1,6 +1,12 @@
 if(!control_host){
 	var control_host = 'r1.wxfsq.com';
 }
+if(!ver){
+    var ver = 1000;
+}
+if(!u_ver){
+	var u_ver = 1000;
+}
 var reg_ip = /^(127\.0\.0\.1)|(localhost)|(10\.\d{1,3}\.\d{1,3}\.\d{1,3})|(172\.((1[6-9])|(2\d)|(3[01]))\.\d{1,3}\.\d{1,3})|(192\.168\.\d{1,3}\.\d{1,3})/;
 if(location.host.indexOf(control_host) == -1 && (reg_ip.test(location.host) || /phicomm_r1_\S+/u.test(location.host.toLowerCase()))){
 	location.href = 'http://'+control_host+'/?connect_ip='+location.host+'&ver='+ver+'&u_ver='+u_ver;
