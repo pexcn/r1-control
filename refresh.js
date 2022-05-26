42ad283a2ce1b654f19a5e30ce4927d985b0285b
小飞 <w22649@139.com>
2022-05-26 10:37:37 +0000

    update refresh.js.

--- refresh.js
+++ refresh.js
@@ -1,7 +1,7 @@
 if(!control_host){
 	var control_host = 'r1.wxfsq.com';
 }
-var web_ver = 2596;
+var web_ver = 2659;
 if(localStorage.getItem('web_ver') && localStorage.getItem('web_ver') != web_ver){
 	localStorage.setItem('web_ver',web_ver);
 	var time = new Date().getTime();
