c633b65c69a5a1b3921dcd05134985eb99478b43
pexcn <pexcn97@gmail.com>
2024-10-02 18:22:27 +0800

    remove baidu statistics

--- js/r1_control.js
+++ js/r1_control.js
@@ -8180,22 +8180,6 @@ function load() {
 	div.style.display = 'none';
 	main_div.appendChild(div);
 
-	if (typeof (baidu_hm_id) == 'string') {
-		(function () {
-			var hm = document.createElement("script");
-			hm.src = location.protocol + "//hm.baidu.com/hm.js?" + baidu_hm_id;
-			hm.onload = function () {
-				no_referrer();
-			};
-			hm.onerror = function () {
-				no_referrer();
-			};
-			main_div.appendChild(hm);
-		})();
-	} else {
-		no_referrer();
-	}
-
 	if (location.href.indexOf('debug') > -1) {
 		load_console();
 	}
